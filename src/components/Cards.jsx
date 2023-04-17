d51d4c81cb57022a9141428420ccecaa0edc73ff
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-04-17 07:01:21 +0400

    reset app and created card

---
+++ src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from './Card'
+
+const Cards = () => {
+  return(
+    <div className="cards-wrapper">
+      <Card />
+    </div>
+  )
+}
+
+export default Cards
