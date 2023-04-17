03172352fc4c63556303bfbbd942aab1478d660e
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-04-17 07:07:26 +0400

    linked the cards into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './App.css'
+import Cards from "./components/Cards";
 
 function App() {
   return (
-    <div className="card-wrapper"></div>
+    <div className="card-wrapper">
+      <Cards />
+    </div>
   )
 }
 
